9c9320b9e0a11224a6053b0fea416373c134bbc4
John Alexis Guerra Gómez <john.guerra@gmail.com>
2025-02-04 12:17:09 -0800

    Showing listings from a local variable

---
+++ frontend/js/client.js
@@ -0,0 +1,37 @@
+console.log("⭐️ client.js loaded");
+
+function DiaryMaster() {
+  let entries = [];
+  const entriesDiv = document.getElementById("divEntries");
+
+  async function getEntries() {
+    const res = await fetch("/api/entries");
+    const data  = await res.json();
+    console.log("⭐️ Got entries", data);
+    entries = data.entries;
+
+    renderEntries();
+  }
+
+  function renderEntries() {
+    entriesDiv.innerHTML = entries
+      .map(
+        (entry) => `<div class="col-md-4 col-xs-12">
+      <div class="card">
+        <div class="card-body">
+          <h3 class="card-title">${entry.date}</h3>
+          <p class="card-text">${entry.text}</p>
+          <p class="card-text">${entry.owner}</p>
+          <p class="card-text">${entry.tags.join(", ")}</p>
+        </div>
+      </div>
+
+    </div>`
+      )
+      .join("\n");
+  }
+
+  getEntries();
+}
+
+DiaryMaster();
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -6,6 +7,11 @@ const app = express();
 import entriesRouter from "./routes/entries.js";
 
 app.use("/api/entries/", entriesRouter);
+
+app.use(express.static("frontend"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
